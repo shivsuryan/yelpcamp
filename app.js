d550809a0240e308e26d7b55b2bfd1c3375de1ee
shiv <shivakumarsuryan@gmail.com>
2019-10-20 10:53:24 +0530

    New campground form, code, campgrounds page and anchor to new campgrounds page. Installed body-parser.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var request = require('request');
+var bodyParser = require('body-parser');
 var port = 3000;
+
+var campgrounds = [
+    {name:"Camp01",image:"https://images.unsplash.com/photo-1453787817889-57ec065fa4a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"},
+    {name:"Camp02",image:"https://images.unsplash.com/photo-1534531409543-069f6204c5b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name:"Camp03",image:"https://images.unsplash.com/photo-1416138182074-93cf510477b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
+];
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/",(req,res) => {
@@ -11,12 +19,20 @@ app.get("/",(req,res) => {
 });
 
 app.get("/campgrounds",(req,res) => {
-    var campgrounds = [
-        {name:"Camp01",image:"https://images.unsplash.com/photo-1453787817889-57ec065fa4a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"},
-        {name:"Camp02",image:"https://images.unsplash.com/photo-1534531409543-069f6204c5b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-        {name:"Camp03",image:"https://images.unsplash.com/photo-1416138182074-93cf510477b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
-    ]
+    var newLocal = campgrounds;
     res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.post("/campgrounds",(req,res) => {
+    var campName = req.body.campname;
+    var campUrl = req.body.campurl;
+    campgrounds.push({name:campName,image:campUrl});
+    res.redirect('/campgrounds');
+    //res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.get("/campgrounds/new", function(req,res){
+    res.render("newCampground");
 });
 
 app.get("*", function(req,res){
