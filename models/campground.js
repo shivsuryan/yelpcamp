e717ae06789d32b5e3b1ac6adcfdb0af88f0ce96
shiv <shivakumarsuryan@gmail.com>
2019-10-21 23:06:25 +0530

    Made Comment association.

--- models/campground.js
+++ models/campground.js
@@ -1,9 +1,15 @@
 var mongoose = require('mongoose');
+var Comment = require('./comment');
 
 var campgroundSchema = mongoose.Schema({
     name: String,
     imageUrl: String,
-    description: String
+    description: String,
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }
+    ]
 })
 
 var Campground = mongoose.model("Campground", campgroundSchema);
